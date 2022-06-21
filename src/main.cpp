13ec5bf5147b73a9933905ae19f757513f3e2b89
Kehny <lexou_k@hotmail.com>
2022-06-21 21:37:38 +0200

    PlatformIO initialization

---
+++ src/main.cpp
@@ -0,0 +1,66 @@
+#include <Arduino.h>
+#include <pins.hpp>
+#include <HardwareSerial.h>
+#include <AccelStepper.h>
+#include "math.h"
+#include <Wire.h>
+
+/*
+AccelStepper leftMot;
+int i = 0;
+uint64_t millisLedOn = 0;
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  //pinMode(PIN_MOTOR_G_DIR,OUTPUT);
+  //pinMode(PIN_MOTOR_G_STEP,OUTPUT);
+  pinMode(PIN_MOTOR_ENABLED,OUTPUT);
+  pinMode(PIN_BATT,INPUT);
+  pinMode(LED_BUILTIN,OUTPUT);
+  digitalWrite(PIN_MOTOR_ENABLED,HIGH);
+  //digitalWrite(PIN_MOTOR_G_DIR,HIGH);
+  leftMot = AccelStepper(1,PIN_MOTOR_G_STEP,PIN_MOTOR_G_DIR);
+  leftMot.setMaxSpeed(10000);
+  leftMot.setSpeed(5000);
+  analogReadRes(10);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  leftMot.runSpeed();
+  float period = 5; //s
+  float amplitude = 5000; //tick/s
+  float x = sin(2*3.1415/period*millis()/1000)*amplitude;
+  leftMot.setSpeed(x);
+  if (i%5000==0)
+    Serial.println(analogRead(PIN_BATT)/1023.0*16.6);
+
+  if (millis()-millisLedOn<20)
+    digitalWrite(LED_BUILTIN,HIGH);
+  else
+    digitalWrite(LED_BUILTIN,LOW);
+
+  
+  if (millis()-millisLedOn>1000){
+    digitalWrite(LED_BUILTIN,HIGH);
+    millisLedOn=millis();
+  }
+  
+  i++;
+}
+
+*/
+
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  SERIAL_GPS.begin(9600);
+
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  Serial.println(SERIAL_GPS.readString());
+}
---
+++ src/pins.hpp
@@ -0,0 +1,25 @@
+#ifndef _PINS_HPP
+#define _PINS_HPP
+
+#define PIN_GPS_TX 1
+#define PIN_GPS_RX 2
+#define SERIAL_GPS Serial1
+#define BAUD_GPS 9600
+
+#define PIN_MOTOR_D_STEP 2
+#define PIN_MOTOR_D_DIR 3
+#define PIN_MOTOR_G_STEP 4
+#define PIN_MOTOR_G_DIR 5
+#define PIN_MOTOR_ENABLED 6
+
+#define PIN_BATT 23
+
+#define PIN_BLUETOOTH_TX 20
+#define PIN_BLUETOOTH_RX 21
+#define SERIAL_BLUETOOTH Serial5
+
+#define PIN_IMU_SDA 17
+#define PIN_IMU_SCL 16
+#define WIRE_IMU Wire1
+
+#endif /* _PINS_HPP */
